fc7dc49af9ac7c77868522fb84ca0a1849adfb66
Navjot Singh Saini <157043540+navjot0079@users.noreply.github.com>
2026-02-16 22:14:21 +0530

    LeetCode 55 Jump Game

---
+++ JAVA/DP/JumpGame.java
@@ -0,0 +1,20 @@
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false; // Can't reach this position
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        return true; // Can reach the end
+    }
+    public static void main(String[] args) {
+        JumpGame solution = new JumpGame();
+        int[] nums1 = {2, 3, 1, 1, 4};
+        int[] nums2 = {3, 2, 1, 0, 4};
+        
+        System.out.println(solution.canJump(nums1)); // Output: true
+        System.out.println(solution.canJump(nums2)); // Output: false
+    }
+}
